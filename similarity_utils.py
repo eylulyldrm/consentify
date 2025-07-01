d0771b654162132a0ce77a8344914e37dc28e9b8
eylül <149818988+eylulyldrm@users.noreply.github.com>
2025-07-01 19:20:20 +0300

    Update similarity_utils.py

--- similarity_utils.py
+++ similarity_utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def cosine_similarity(vec1, vec2):
-    """İki vektör arasındaki kosinüs benzerliğini hesaplar."""
+   
     vec1 = np.array(vec1)
     vec2 = np.array(vec2)
     return np.dot(vec1, vec2) / (np.linalg.norm(vec1) * np.linalg.norm(vec2))
